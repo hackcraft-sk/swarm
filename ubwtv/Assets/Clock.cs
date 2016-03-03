bb0f0906e6284ddabfa1603651e271b032497cae
Matej Isteník <matej.istenik@inloop.eu>
2016-03-03 01:48:32 +0100

    Added utc clock display to overlay.

---
+++ ubwtv/Assets/Clock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+
+	private UnityEngine.UI.Text clockText;
+
+	void Start () {
+		clockText = GetComponent<UnityEngine.UI.Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DateTime utcTime = DateTime.UtcNow;
+		string utcTimeString = String.Format("{0:D2}:{1:D2}:{2:D2} UTC", utcTime.Hour, utcTime.Minute, utcTime.Second);
+		clockText.text = utcTimeString;
+	}
+}
