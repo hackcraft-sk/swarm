01ab3eff286d030fe9d1cfe453cef5a7a0f4587e
Matej Isteník <matej.istenik@inloop.eu>
2016-03-04 22:14:03 +0100

    Added banner picker component system.

---
+++ ubwtv/Assets/BannerPicker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class BannerPicker : MonoBehaviour
+{
+	public Texture[] banners;
+
+	public Sprite targetSprite;
+
+	int bannerIndex = -1;
+
+	public void setNextBanner() {
+		bannerIndex = bannerIndex + 1;
+		bannerIndex %= banners.Length;
+
+		Texture bannerTexture = banners [bannerIndex];
+		targetSprite.texture = bannerTexture;
+	}
+}
--- ubwtv/Assets/Clock.cs
+++ ubwtv/Assets/Clock.cs
@@ -9,8 +9,7 @@ public class Clock : MonoBehaviour {
 	void Start () {
 		clockText = GetComponent<UnityEngine.UI.Text> ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		DateTime utcTime = DateTime.UtcNow;
 		string utcTimeString = String.Format("{0:D2}:{1:D2}:{2:D2} UTC", utcTime.Hour, utcTime.Minute, utcTime.Second);
